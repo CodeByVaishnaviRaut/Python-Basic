43bb940ba82648727dfe994d10dc65852bac33ce
CodeByVaishnaviRaut <160324454+CodeByVaishnaviRaut@users.noreply.github.com>
2024-02-21 09:41:29 +0530

    Update Solution.py

--- Lists/Solution.py
+++ Lists/Solution.py
@@ -1,17 +1,15 @@
-if __name__ == '__main__':
-    N = int(input())
-    list1 = []
+lst = []
+
     command_map = {
-        'insert':lambda x:list1.insert(int(x[1]),int(x[2])),
-        'print': lambda x: print(x),
-        'remove': lambda x: list1.remove(int(x[1])),
-        'append':lambda x: list1.append(int(x[1])),
-        'sort':lambda x: list1.sort(),
-        'pop':lambda x: list1.pop(),
-        'reverse':lambda x: list1.reverse()
+        'insert': lambda x: lst.insert(int(x[1]), int(x[2])),
+        'print': lambda x: print(lst),
+        'remove': lambda x: lst.remove(int(x[1])),
+        'append': lambda x: lst.append(int(x[1])),
+        'sort': lambda x: lst.sort(),
+        'pop': lambda x: lst.pop(),
+        'reverse': lambda x: lst.reverse()
     }
-    
-    for i in range(N):
+
+    for _ in range(N):
         command = input().split()
         command_map[command[0]](command)
-    
