81af3f26222078188ca9b1532a8dd59bacd515f1
CodeByVaishnaviRaut <160324454+CodeByVaishnaviRaut@users.noreply.github.com>
2024-02-23 16:19:52 +0530

    Update Solution.py

--- Design Door Mat/Solution.py
+++ Design Door Mat/Solution.py
@@ -1,17 +1,22 @@
-# Enter your code here. Read input from STDIN. Print output to STDOUT
-n,m = map(int,input().split())
-pattern = ".|."
+def print_door_mat(n, m):
+    pattern = ".|."
+    welcome_line = "WELCOME"
 
-for i in range(n):
-        pattern = '.|.'
-        if i < (n - 1) / 2:
-            # Calculate the number of dots and pipes before the center line
-            print((pattern * i).center(m, '-'))
-        elif i == (n - 1) / 2:
-            # Print the center line with the word "WELCOME"
-            print('WELCOME'.center(m, '-'))
+    for i in range(n):
+        if i < (n - 1) // 2:
+            # Calculate the number of dashes on each side of the pattern
+            dashes = (m - (len(pattern) * (2 * i + 1))) // 2
+            print("-" * dashes + pattern * (2 * i + 1) + "-" * dashes)
+        elif i == (n - 1) // 2:
+            # Calculate the number of dashes on each side of the "WELCOME" line
+            dashes = (m - len(welcome_line)) // 2
+            print("-" * dashes + welcome_line + "-" * dashes)
         else:
-            # Calculate the number of dots and pipes after the center line
-            print((pattern * (n - 1 - i)).center(m, '-'))
+            # Calculate the number of dashes on each side of the pattern
+            dashes = (m - (len(pattern) * (2 * (n - 1 - i) + 1))) // 2
+            print("-" * dashes + pattern * (2 * (n - 1 - i) + 1) + "-" * dashes)
 
 
+# Example: Size 7 x 21
+n,m = map(int,input().split())
+print_door_mat(n,m)
